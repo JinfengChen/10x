03073e9eaf9363b684e0490f77ac6e49b6ca233b
JinfengChen <jinfeng7chen@gmail.com>
2017-05-18 15:22:39 -0700

    contig graph

---
+++ bin/contig_gragh/Step0_map_blasr.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=24
+#SBATCH --mem=100G
+#SBATCH --time=40:00:00
+#SBATCH --output=run_speedseq_qsub.sh.stdout
+#SBATCH -p intel
+#SBATCH --workdir=./
+
+start=`date +%s`
+
+module load blasr/5.2
+
+#perl ~/BigData/software/bin/make_contigs_from_fasta.pl Fairchild.fasta
+
+#ref=/rhome/cjinfeng/BigData/00.RD/Assembly/Pacbio/Reference/Csinensis_HZAU/Citrus_HZAU.fa
+#reads=/rhome/cjinfeng/BigData/00.RD/Assembly/Pacbio/PBcR/Pacbio_Citrus_32c_128G/Citrus/9-terminator/asm.scf.fasta
+ref=Fairchild.fasta_contig.fasta
+reads=Citrus10x.fasta_contig.10kb.fasta
+#/opt/blasr/453c25ab/bin//sawriter $ref.sa $ref
+#$blasr $reads $ref -sa $ref.sa -rbao -minMatch 40 -advanceExactMatches 40 -noRefineAlignments -affineAlign -affineOpen 100 -affineExtend 0 -sam -nproc 2 | /usr/local/bin/samtools view -bhS - > Citrus_ctg2genome.blasr.raw.bam
+blasr $reads $ref -m 1 --nproc 24 > blasr.output.m1
+#/usr/local/bin/samtools sort -m 1000000000 Citrus.blasr.raw.bam Citrus.corrected.blasr
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "Start: $start"
+echo "End: $end"
+echo "Run time: $runtime"
+
+echo "Done"
+
---
+++ bin/contig_gragh/Step1_graph_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=24
+#SBATCH --mem=100G
+#SBATCH --time=40:00:00
+#SBATCH --output=run_speedseq_qsub.sh.stdout
+#SBATCH -p intel
+#SBATCH --workdir=./
+
+start=`date +%s`
+
+module load blasr/5.2
+
+align=test.blasr.out
+ref=Fairchild.fasta_contig.fasta
+reads=Citrus10x.fasta_contig.10kb.fasta
+script=/rhome/cjinfeng/BigData/00.RD/Assembly/10xgenomics/tools/ncomms15324-s10/
+output=fairchild_graph
+perl ../../tools/ncomms15324-s10/Fragment_Guided_Graph_Assembly.pl -Align=$align -Ref=$ref -Query=$reads -Script=$script -OutDir=$output 
+chmod 755 Graph_Assembly.sh 
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "Start: $start"
+echo "End: $end"
+echo "Run time: $runtime"
+
+echo "Done"
+
