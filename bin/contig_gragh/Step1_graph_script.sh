e3147bb829d23f45c0b7a992308675ae74b40d61
JinfengChen <jinfeng7chen@gmail.com>
2017-05-18 20:38:47 -0700

    make contig from fasta

--- bin/contig_gragh/Step1_graph_script.sh
+++ bin/contig_gragh/Step1_graph_script.sh
@@ -16,8 +16,9 @@ ref=Fairchild.fasta_contig.fasta
 reads=Citrus10x.fasta_contig.10kb.fasta
 script=/rhome/cjinfeng/BigData/00.RD/Assembly/10xgenomics/tools/ncomms15324-s10/
 output=fairchild_graph
-perl ../../tools/ncomms15324-s10/Fragment_Guided_Graph_Assembly.pl -Align=$align -Ref=$ref -Query=$reads -Script=$script -OutDir=$output 
+perl ../../tools/ncomms15324-s10/Fragment_Guided_Graph_Assembly.pl -Align=$align -Ref=$ref -Query=$reads -Script=$script -OutDir=$output -MinIden=99 -MaxHang=1000 -MinLap=5000 
 chmod 755 Graph_Assembly.sh 
+sbatch Graph_Assembly.sh
 
 end=`date +%s`
 runtime=$((end-start))
--- bin/contig_gragh/work.sh
+++ bin/contig_gragh/work.sh
@@ -1,14 +1,13 @@
 
 ln -s /rhome/cjinfeng/BigData/00.RD/Assembly/10xgenomics/bin/supernovo/Citrus10x.fasta ./
 ln -s /rhome/cjinfeng/Rice/Rice_population_sequence/BUSCO/Fairchild_falconv3_20kb_cov2_p_ctg_quiver_round1_pilon_haplomerge.fasta Fairchild.fasta
-perl ~/BigData/software/bin/make_contigs_from_fasta.pl Fairchild.fasta
+perl make_contigs_from_fasta_1line.pl Fairchild.fasta
 perl ~/BigData/software/bin/fastaDeal.pl --attr id:len Fairchild.fasta_contig.fasta > Fairchild.fasta_contig.fasta.len
-perl ~/BigData/software/bin/make_contigs_from_fasta.pl Citrus10x.fasta
+perl make_contigs_from_fasta_1line.pl Citrus10x.fasta
 perl ~/BigData/software/bin/fastaDeal.pl --attr id:len Citrus10x.fasta_contig.fasta > Citrus10x.fasta_contig.fasta.len
 awk '$2>10000' Citrus10x.fasta_contig.fasta.len | cut -f1 > Citrus10x.fasta_contig.10kb.id
 perl getidseq.pl -l Citrus10x.fasta_contig.10kb.id -f Citrus10x.fasta_contig.fasta -o Citrus10x.fasta_contig.10kb.fasta
 
-perl ~/BigData/software/bin/fastaDeal.pl --attr id:len Citrus10x.fasta > Citrus10x.fasta.len
-awk '$2>10000' Citrus10x.fasta.len | cut -f1 > Citrus10x.10kb.id
-perl getidseq.pl -l Citrus10x.10kb.id -f Citrus10x.fasta -o Citrus10x.10kb.fasta
+sbatch Step0_map_blasr.sh
+bash Step1_graph_script.sh
 
